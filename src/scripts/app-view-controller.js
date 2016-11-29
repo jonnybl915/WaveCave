ba65e6f5d3dde1ebdf02322e8e83285d4a11a62c
jonathandavidblack <jonnybl915@gmail.com>
2016-11-29 14:38:39 -0500

    added app-view-controller.js file, and added AppRouter to app.js file

---
+++ src/scripts/app-view-controller.js
@@ -0,0 +1,25 @@
+import React, {Component, PropTypes} from 'react'
+import HomeView from './homeview.component.js'
+
+class AppViewController extends Component {
+
+  constructor(props) {
+  super(props);
+  }
+
+  render: function(){
+
+        switch (this.props.routedFrom){
+
+           case "HomeView":
+              return <HomeView/>
+              break;
+
+           case "MakeFilterView":
+              return <MakeFilterView/>
+              break;
+        }
+     }
+}
+
+module.exports = AppViewController
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,26 @@
-const React = require('react')
-const ReactDom = require('react-dom')
+import React, {Component, PropTypes} from 'react'
+import ReactDOM from 'react-dom'
+import Backbone from 'backbone'
+import ViewTemplate from './view-controller.js'
+
+const AppRouter = Backbone.Router.extend({
+  routes: {
+    "" : "ShowHomeView",
+    "makeFilterPage" : "ShowMakeFilterPage"
+  },
+
+  ShowHomeView: function() {
+    console.log("hitting home page")
+    ReactDOM.render(<ViewTemplate/>)
+  },
+
+  ShowMakeFilterPage: function(){
+
+  },
+
+  initialize: function(){
+    Backbone.history.start();
+  }
+})
+
+let app = new AppRouter;
