4ed2d9b2efc88b3d3cadd79e2e7362886fa727c8
jonathandavidblack <jonnybl915@gmail.com>
2016-11-29 14:52:17 -0500

    added basic actions.js file

---
+++ src/scripts/actions.js
@@ -0,0 +1,17 @@
+import Backbone from 'backbone'
+import STORE from './store.js';
+
+
+//imort models here
+
+const ACTIONS = {
+
+//  fetchFilterCollection: function(queryObject){
+//  },
+
+//  createNewFilter: function(newDataToPost){
+//  },
+
+}
+
+export default ACTIONS
