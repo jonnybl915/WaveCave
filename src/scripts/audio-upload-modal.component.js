ed71a31a6b3120aec393c0e86726654e39706c35
jonathandavidblack <jonnybl915@gmail.com>
2016-11-30 21:35:03 -0500

    began audio upload modal

---
+++ src/scripts/audio-upload-modal.component.js
@@ -0,0 +1,16 @@
+import React, {Component, PropTypes} from 'react'
+
+export default class AudioUploadModal extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="audioModalBox">
+           <h6>Drag Audiofile To Upload</h6>
+           <input></input>
+           <img src=""></img>
+           </div>
+  }
+}
